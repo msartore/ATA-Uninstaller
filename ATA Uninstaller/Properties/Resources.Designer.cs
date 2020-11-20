3d1482a4cfcb94237ca5bc2c5442db422c0171fb
Massimiliano Sartore <massimiliano.sartore@protonmail.com>
2020-11-20 23:07:10 +0100

    remove useless code

--- ATA Uninstaller/Properties/Resources.Designer.cs
+++ ATA Uninstaller/Properties/Resources.Designer.cs
@@ -27,7 +27,11 @@ namespace ATA_Uninstaller.Properties {
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources() {
+        }
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
@@ -39,6 +43,20 @@ namespace ATA_Uninstaller.Properties {
                     resourceMan = temp;
                 }
                 return resourceMan;
+            }
+        }
+        
+        /// <summary>
+        ///   Overrides the current thread's CurrentUICulture property for all
+        ///   resource lookups using this strongly typed resource class.
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
+                return resourceCulture;
+            }
+            set {
+                resourceCulture = value;
             }
         }
         
